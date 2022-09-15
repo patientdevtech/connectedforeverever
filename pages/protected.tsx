789edc25ad15196896bf42314b8824abeea0c129
patientdevtech <patientdevtech@gmail.com>
2022-09-14 20:46:47 -0400

    protected.tsx updated to comment out await fetch from protected route

--- pages/protected.tsx
+++ pages/protected.tsx
@@ -9,16 +9,16 @@ export default function ProtectedPage() {
   const [content, setContent] = useState()
 
   // Fetch content from protected route
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("/api/examples/protected")
-      const json = await res.json()
-      if (json.content) {
-        setContent(json.content)
-      }
-    }
-    fetchData()
-  }, [session])
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const res = await fetch("/api/examples/protected")
+//       const json = await res.json()
+//       if (json.content) {
+//         setContent(json.content)
+//       }
+//     }
+//     fetchData()
+//   }, [session])
 
   // When rendering client side don't display anything until loading is complete
   if (typeof window !== "undefined" && loading) return null
