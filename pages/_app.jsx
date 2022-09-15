caad6ccecac4fd6ebdd58ade93e24f62de0fb2fa
patientdevtech <patientdevtech@gmail.com>
2022-09-15 07:20:48 -0400

    trying out new _app.jsx export default function from nextauth.js

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,15 +2,55 @@ import '../styles/globals.css'
 import { SessionProvider } from "next-auth/react"
 // import type { AppProps } from "next/app"
 
-// Use of the <SessionProvider> is mandatory to allow components that call
-// `useSession()` anywhere in your application to access the `session` object.
-export default function App({ Component, pageProps }) {
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <SessionProvider session={pageProps.session} refetchInterval={0}>
-      <Component {...pageProps} />
+    <SessionProvider session={session}>
+      {Component.auth ? (
+        <Auth>
+          <Component {...pageProps} />
+        </Auth>
+      ) : (
+        <Component {...pageProps} />
+      )}
     </SessionProvider>
   )
 }
+
+function Auth({ children }) {
+  // if `{ required: true }` is supplied, `status` can only be "loading" or "authenticated"
+  const { status } = useSession({ required: true })
+
+  if (status === "loading") {
+    return <div>Loading...</div>
+  }
+
+  return children
+}
+
+
+
+
+
+
+
+
+// resource: https://next-auth.js.org/getting-started/client
+
+
+
+// Use of the <SessionProvider> is mandatory to allow components that call
+// `useSession()` anywhere in your application to access the `session` object.
+// export default function App({ Component, pageProps }) {
+//   return (
+//     <SessionProvider session={pageProps.session} refetchInterval={0}>
+//       <Component {...pageProps} />
+//     </SessionProvider>
+//   )
+// }
 
 // https://github.com/nextauthjs/next-auth-example/blob/main/pages/_app.tsx
 
